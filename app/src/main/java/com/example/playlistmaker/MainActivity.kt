ed9a453e9d436681d24d002d58fa721a278c16a7
Sergey Orlov <master1origami@yandex.ru>
2023-02-05 21:07:31 +0300

    Второй коммит, второе домашнее задание 8-го спринта: На главном экране приложения реализовал нажатия на кнопки с тостами. Для кнопки "Поиск" реализовал с помощью анонимного класса, для кнопок "Медиатека" и "Настройки" через лямбда-выражение. Также добавил в XML коде кнопок эффект нажатия ripple effect

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -2,13 +2,28 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
+        val search = findViewById<Button>(R.id.search_button)
+        val searchClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Toast.makeText(this@MainActivity, "Начинаем искать!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        val library = findViewById<Button>(R.id.library_button)
+        library.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Заходим в медиатеку", Toast.LENGTH_SHORT).show()
+        }
+        val settings = findViewById<Button>(R.id.settings_button)
+        settings.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Заходим в настройки", Toast.LENGTH_SHORT).show()
+        }
+        search.setOnClickListener(searchClickListener)
     }
 }
