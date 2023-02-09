6274f557f6339b06feadea958bb719337c2f425b
Sergey Orlov <master1origami@yandex.ru>
2023-02-09 22:51:06 +0300

    Третий коммит, третье домашнее задание 8-го спринта: Пересоздал SettingsActivity, создал пустые Activity для Поиска и Медиатеки. Все Activity объявил в AndroidManifest.xml Убрал тосты из обработчиков нажатия на кнопки и добавил код в классы всех Activity для перехода из главного экрана в Поиск, Медиатеку и Настройки. На всех экранах добавил обработчик нажатий на кнопку "Назад", чтобы можно было вернуться из каждой Activity на главный экран. Немного поправил XML разметку.

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,29 +1,31 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val search = findViewById<Button>(R.id.search_button)
-        val searchClickListener: View.OnClickListener = object : View.OnClickListener {
-            override fun onClick(p0: View?) {
-                Toast.makeText(this@MainActivity, "Начинаем искать!", Toast.LENGTH_SHORT).show()
-            }
+
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val libraryButton = findViewById<Button>(R.id.library_button)
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+
+        searchButton.setOnClickListener {
+            val searchIntent = Intent(this, SearchActivity::class.java)
+            startActivity(searchIntent)
         }
-        val library = findViewById<Button>(R.id.library_button)
-        library.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Заходим в медиатеку", Toast.LENGTH_SHORT).show()
+        libraryButton.setOnClickListener {
+            val libraryIntent = Intent(this, MediaLibraryActivity::class.java)
+            startActivity(libraryIntent)
         }
-        val settings = findViewById<Button>(R.id.settings_button)
-        settings.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Заходим в настройки", Toast.LENGTH_SHORT).show()
+        settingsButton.setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
         }
-        search.setOnClickListener(searchClickListener)
     }
 }
---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,20 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val arrowBack = findViewById<ImageView>(R.id.back_arrow)
+        arrowBack.setOnClickListener {
+            val arrowIntent = Intent(this, MainActivity::class.java)
+            startActivity(arrowIntent)
+        }
+
+    }
+}
