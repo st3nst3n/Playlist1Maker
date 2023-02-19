16d7d95c0c36a0f949ce09c81eae252151ecb801
Sergey Orlov <master1origami@yandex.ru>
2023-02-19 19:44:49 +0300

    Спринт 9, домашнее задание №2 1) Строки уже вынесены в строковые ресурсы strings.xml 2) Реализовал кнопку "Поделиться приложением". 3) Реализовал кнопку "Напиать в поддержку" 4) Реализовал кнопку "Пользовательское соглашение"

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,9 +1,11 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,35 @@ class SettingsActivity : AppCompatActivity() {
             val arrowIntent = Intent(this, MainActivity::class.java)
             startActivity(arrowIntent)
             finish()
+        }
+
+        val shareApp = findViewById<TextView>(R.id.shareButton)
+        shareApp.setOnClickListener {
+            val shareText = "https://practicum.yandex.ru/profile/android-developer/"
+            val sendText = Intent(Intent.ACTION_SEND)
+            sendText.putExtra(Intent.EXTRA_TEXT, shareText)
+            sendText.type = "text/plain"
+            val shareIntent = Intent.createChooser(sendText, null)
+            startActivity(shareIntent)
+        }
+
+        val sendToSupport = findViewById<TextView>(R.id.supportButton)
+        sendToSupport.setOnClickListener {
+            val lineTitle = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val lineMessage = "Спасибо разработчикам и разработчицам за крутое приложение!"
+            val shareIntent = Intent(Intent.ACTION_SENDTO)
+            shareIntent.data = Uri.parse("mailto:")
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("master1origami@yandex.ru"))
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, lineTitle)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, lineMessage)
+            startActivity(shareIntent)
+        }
+
+        val readTerms = findViewById<TextView>(R.id.termsButton)
+        readTerms.setOnClickListener {
+            val openTerms = Intent(Intent.ACTION_VIEW)
+            openTerms.data = Uri.parse("https://yandex.ru/legal/practicum_offer/")
+            startActivity(openTerms)
         }
 
     }
